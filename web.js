396f97fd1c3f8306ebc05d9bdf36a1c6dc532b88
santhosh-saminathan <36120220+santhosh-saminathan@users.noreply.github.com>
2018-02-15 18:21:52 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,32 @@
+'use strict';
+var express = require('express');
+var app = express();
+// var bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', (process.env.PORT || 5000));
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,webToken');
+
+    if ('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+app.get('/test',function(req,res){
+    res.send("Hello world");
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log('Express server started');
+});
+
+module.exports = app
